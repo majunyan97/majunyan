81a591cb974d3708e75a366a60ee3f787c83da56
majunyan97 <83214620+majunyan97@users.noreply.github.com>
2021-04-27 00:13:31 +0800

    第一次游戏制作

---
+++ New Unity Project (1)/Assets/PlayContral.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayContral : MonoBehaviour
+{
+    private Rigidbody2D Hero;
+    public float force = 100;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Hero = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        //Hero.AddForce(Vector2.right * h * force);  //Vector2.right => new Vector( 1, 0 );
+        Hero.velocity = new Vector2(h * 20, Hero.velocity.y);
+        
+    }
+}
